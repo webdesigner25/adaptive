a6bbdbb7ff457cb759354746f89cab4c704e51bb
webdesigner25 <shmatko.t.v@gmail.com>
2022-09-04 20:16:26 +0300

    addscript

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const btn__burger = document.querySelector('#btn-burger');
+const btn__close = document.querySelector('#btn-close');
+const header = document.querySelector('#header');
+
+
+btn__burger.addEventListener('click', function(){
+    header.classList.add('menu-open');
+
+    btn__close.addEventListener('click', function(){
+
+        header.classList.replace('menu-open', 'header')
+    })
+})
